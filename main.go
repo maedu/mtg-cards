03166e72944513a06e1ae7e563d5dd815498c9e0
Matthias Spinner <matthias@spinner-web.ch>
2020-12-06 15:21:59 +0000

    cleanup

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 
 	env "bitbucket.org/spinnerweb/accounting_common/env"
 	cardApi "bitbucket.org/spinnerweb/cards/card/api"
-	game "bitbucket.org/spinnerweb/cards/draft/booster/game"
 	"bitbucket.org/spinnerweb/cards/draft/sealed"
 	"bitbucket.org/spinnerweb/cards/server"
 )
@@ -14,6 +13,5 @@ func main() {
 	server := server.Configure()
 	cardApi.Setup(server)
 	sealed.Setup(server)
-	game.Setup(server)
 	server.Run(fmt.Sprintf("0.0.0.0:%s", env.GetEnv("SERVER_PORT", "4004"))) // listen and serve on 0.0.0.0:8080
 }
