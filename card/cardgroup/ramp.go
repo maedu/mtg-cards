42812b0613dd584c882298b6bc3d62ebaabc9cba
Matthias Spinner <matthias@spinner-web.ch>
2021-01-02 00:01:18 +0000

    filters

--- card/cardgroup/ramp.go
+++ card/cardgroup/ramp.go
@@ -8,7 +8,7 @@ import (
 )
 
 func calculateRamp(card *db.Card) {
-	if isManaArtifact(card) {
+	if isManaArtifact(card) || isRampCard(card) {
 		card.CardGroups = append(card.CardGroups, "Ramp")
 		card.SearchText = fmt.Sprintf("%s, ramp", card.SearchText)
 	}
@@ -16,10 +16,25 @@ func calculateRamp(card *db.Card) {
 
 var rampManaRegex, _ = regexp.Compile("\\{T\\}: Add (\\{[CBUGRW]\\}|[a-zA-Z ]+mana)")
 
+var rampCards = []string{
+	"Jeweled Lotus",
+	"Explorer's Scope",
+	"Horizon Stone",
+}
+
 func isManaArtifact(card *db.Card) bool {
 	if card.CardType != db.Artifact {
 		return false
 	}
 
 	return rampManaRegex.MatchString(card.OracleText)
+}
+
+func isRampCard(card *db.Card) bool {
+	for _, rampCard := range rampCards {
+		if rampCard == card.Name {
+			return true
+		}
+	}
+	return false
 }
