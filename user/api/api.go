41ac8bcbc1f2389ef162b101f26f1d7028e005ec
Matthias Spinner <matthias@spinner-web.ch>
2021-07-20 14:14:30 +0800

    rename user

--- user/api/api.go
+++ user/api/api.go
@@ -1,28 +1,160 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 	"github.com/maedu/mtg-cards/user/auth"
-	userCardDB "github.com/maedu/mtg-cards/user/db"
+	"github.com/maedu/mtg-cards/user/db"
+	"github.com/maedu/mtg-cards/util"
 )
+
+var usernamePattern, _ = regexp.Compile("^[a-zA-Z0-9-]+$")
 
 // Setup Setup REST API
 func Setup(r *gin.Engine) {
 	r.GET("/api/user", handleGetUser)
+	r.POST("/api/user/rename/:newName", handleRenameUser)
 	r.GET("/api/user/cards", handleGetCards)
 }
 
 func handleGetUser(c *gin.Context) {
 	if userID, ok := auth.GetUserIDFromAccessToken(c, true); ok {
-		c.JSON(http.StatusOK, userID)
+		collection, err := db.GetUserCollection()
+		defer collection.Disconnect()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		user, err := collection.GetUserByUserID(userID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		if user == nil {
+			userName, err := calcUserName(userID, &collection)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, err)
+				return
+			}
+			user = &db.User{
+				UserID:   userID,
+				UserName: userName,
+			}
+			_, err = collection.Create(user)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, err)
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, user)
+		return
 	}
+	c.JSON(http.StatusUnauthorized, nil)
+}
+
+func handleRenameUser(c *gin.Context) {
+	if userID, ok := auth.GetUserIDFromAccessToken(c, true); ok {
+
+		collection, err := db.GetUserCollection()
+		defer collection.Disconnect()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		userName := c.Param("newName")
+		if !validUsername(userName) {
+			c.JSON(http.StatusBadRequest, "Username is invalid. Min lenght is 5 and only letter, numbers, - and _ allowed.")
+			return
+		}
+
+		available, err := usernameAvailable(userName, userID, &collection)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		if !available {
+			c.JSON(http.StatusBadRequest, "Username already taken")
+			return
+		}
+
+		user, err := collection.GetUserByUserID(userID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return
+		}
+
+		if user == nil {
+			user = &db.User{
+				UserID:   userID,
+				UserName: userName,
+			}
+			_, err = collection.Create(user)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, err)
+				return
+			}
+		} else {
+			user.UserName = userName
+			user, err = collection.Update(user)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, err)
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, user)
+		return
+	}
+	c.JSON(http.StatusUnauthorized, nil)
+}
+
+func calcUserName(userID string, collection *db.UserCollection) (string, error) {
+	var userName string
+	for {
+		userName = fmt.Sprintf("user-%s", util.RandomString(5))
+		available, err := usernameAvailable(userName, userID, collection)
+		if err != nil {
+			return "", err
+		}
+		if available {
+			break
+		}
+	}
+	return userName, nil
+}
+
+func validUsername(userName string) bool {
+	if len(userName) < 5 {
+		return false
+	}
+	return usernamePattern.MatchString(userName)
+}
+
+func usernameAvailable(userName string, userID string, collection *db.UserCollection) (bool, error) {
+	user, err := collection.GetUserByUserName(userName)
+	if err != nil {
+		return true, err
+	}
+	if user == nil {
+		return true, nil
+	}
+	if user.UserID == userID {
+		return true, nil
+	}
+
+	return false, nil
 }
 
 func handleGetCards(c *gin.Context) {
 	if userID, ok := auth.GetUserIDFromAccessToken(c, true); ok {
-		collection, err := userCardDB.GetUserCardCollection()
+		collection, err := db.GetUserCardCollection()
 		defer collection.Disconnect()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, err)
---
+++ user/db/user_db.go
@@ -0,0 +1,152 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	"github.com/maedu/mtg-cards/db"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id" json:"-"`
+	UserID   string             `bson:"user_id" json:"userId"`
+	UserName string             `bson:"user_name" json:"userName"`
+}
+
+// UserCollection ...
+type UserCollection struct {
+	*mongo.Client
+	*mongo.Collection
+	context.Context
+	context.CancelFunc
+}
+
+// GetUserCollection ...
+func GetUserCollection() (UserCollection, error) {
+	client, ctx, cancel := db.GetConnection()
+	db := client.Database(db.GetDatabaseName())
+	collection := db.Collection("users")
+
+	model := mongo.IndexModel{
+		Keys: bson.M{
+			"user_id": 1,
+		}, Options: nil,
+	}
+	_, err := collection.Indexes().CreateOne(ctx, model)
+
+	return UserCollection{
+		Client:     client,
+		Collection: collection,
+		Context:    ctx,
+		CancelFunc: cancel,
+	}, err
+}
+
+// Disconnect ...
+func (collection *UserCollection) Disconnect() {
+	collection.CancelFunc()
+	collection.Client.Disconnect(collection.Context)
+}
+
+// GetAllUsers Retrives all users from the db
+func (collection *UserCollection) GetAllUsers() ([]*User, error) {
+	var users []*User = []*User{}
+	ctx := collection.Context
+
+	cursor, err := collection.Collection.Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	err = cursor.All(ctx, &users)
+	if err != nil {
+		log.Printf("Failed marshalling %v", err)
+		return nil, err
+	}
+	return users, nil
+}
+
+// GetUserByUserID retrives a user by its user id (aka email)
+func (collection *UserCollection) GetUserByUserID(userID string) (*User, error) {
+	var user *User
+	ctx := collection.Context
+
+	result := collection.Collection.FindOne(ctx, bson.D{bson.E{Key: "user_id", Value: userID}})
+	if result == nil {
+		return nil, nil
+	}
+	err := result.Decode(&user)
+
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+
+	if err != nil {
+		log.Printf("Failed marshalling %v", err)
+		return nil, err
+	}
+	return user, nil
+}
+
+// GetUserByUserID retrives a user by its user id (aka email)
+func (collection *UserCollection) GetUserByUserName(userName string) (*User, error) {
+	var user *User
+	ctx := collection.Context
+
+	result := collection.Collection.FindOne(ctx, bson.D{bson.E{Key: "user_name", Value: userName}})
+	if result == nil {
+		return nil, nil
+	}
+	err := result.Decode(&user)
+
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+
+	if err != nil {
+		log.Printf("Failed marshalling %v", err)
+		return nil, err
+	}
+	return user, nil
+}
+
+// Create creating a user in a mongo
+func (collection *UserCollection) Create(user *User) (primitive.ObjectID, error) {
+	ctx := collection.Context
+	user.ID = primitive.NewObjectID()
+
+	result, err := collection.Collection.InsertOne(ctx, user)
+	if err != nil {
+		log.Printf("Could not create User: %v", err)
+		return primitive.NilObjectID, err
+	}
+	oid := result.InsertedID.(primitive.ObjectID)
+	return oid, nil
+}
+
+func (collection *UserCollection) Update(user *User) (*User, error) {
+	ctx := collection.Context
+	var updatedUser *User
+
+	update := bson.M{
+		"$set": user,
+	}
+
+	upsert := true
+	after := options.After
+	opt := options.FindOneAndUpdateOptions{
+		Upsert:         &upsert,
+		ReturnDocument: &after,
+	}
+
+	err := collection.Collection.FindOneAndUpdate(ctx, bson.M{"_id": user.ID}, update, &opt).Decode(&updatedUser)
+	if err != nil {
+		log.Printf("Could not save User: %v", err)
+		return nil, err
+	}
+	return updatedUser, nil
+}
